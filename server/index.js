414a699eb72006518677651f57fb7480d97437b9
Pritam Khose <pritamkhose@gmail.com>
2021-06-09 20:41:56 +0530

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,85 @@
+const express = require("express");
+var cors = require("cors");
+var app = express();
+
+app.use(cors());
+app.use(express.json());
+const port = 8080;
+var tutorials = require("./tutorials.json");
+
+app.get("/", (req, res) => {
+  return res.send("Hello World!");
+});
+
+app.get("/api/tutorials", cors(), (req, res) => {
+  var title = req.query.title;
+  if (title !== undefined && title !== null && title.length > 0) {
+    const filterTutorials = tutorials.filter((item) =>
+      item.title.toLowerCase().includes(title.toLowerCase())
+    );
+    return res.status(200).json(filterTutorials);
+  } else {
+    return res.json(tutorials);
+  }
+});
+
+app.post("/api/tutorials", cors(), (req, res) => {
+  id = tutorials.length + 1;
+  var tut = {
+    id: id,
+    title: req.body.title,
+    description: req.body.description,
+  };
+  tutorials.push(tut);
+  return res.status(201).json(tut);
+});
+
+app.put("/api/tutorials/:id", cors(), (req, res) => {
+  //get the username from url
+  const id = req.params.id;
+
+  //filter the to update it
+  const filterTutorials = tutorials.filter((item) => item.id.toString() !== id);
+  tutorials = filterTutorials;
+  tutorials.push(req.body);
+  return res.status(200).json(req.body);
+});
+
+app.patch("/api/tutorials", cors(), (req, res) => {
+  tutorials = [];
+  tutorials = require("./tutorials.json");
+  return res.status(200).json(tutorials);
+});
+
+app.get("/api/tutorials/:id", cors(), (req, res) => {
+  //get the username from url
+  const id = req.params.id;
+
+  //filter the tutorials to remove it
+  const filterTutorials = tutorials.filter((item) => item.id.toString() == id);
+  if (tutorials.length === filterTutorials.length) {
+    return res
+      .status(409)
+      .send({ error: true, msg: "tutorials does not exist" });
+  }
+  return res.status(200).json(filterTutorials[0]);
+});
+
+app.delete("/api/tutorials/:id", cors(), (req, res) => {
+  //get the username from url
+  const id = req.params.id;
+
+  //filter the tutorials to remove it
+  const filterTutorials = tutorials.filter((item) => item.id.toString() !== id);
+  tutorials = filterTutorials;
+  return res.status(200).json(filterTutorials);
+});
+
+app.delete("/api/tutorials", cors(), (req, res) => {
+  tutorials = [];
+  return res.json(tutorials);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
---
+++ src/reducers/tutorials.js
@@ -0,0 +1,48 @@
+import {
+  CREATE_TUTORIAL,
+  RETRIEVE_TUTORIALS,
+  UPDATE_TUTORIAL,
+  DELETE_TUTORIAL,
+  DELETE_ALL_TUTORIALS,
+  RESET_ALL_TUTORIALS,
+} from "../actions/types";
+
+const initialState = [];
+
+function tutorialReducer(tutorials = initialState, action) {
+  const { type, payload } = action;
+
+  switch (type) {
+    case CREATE_TUTORIAL:
+      return [...tutorials, payload];
+
+    case RETRIEVE_TUTORIALS:
+      return payload;
+
+    case UPDATE_TUTORIAL:
+      return tutorials.map((tutorial) => {
+        if (tutorial.id === payload.id) {
+          return {
+            ...tutorial,
+            ...payload,
+          };
+        } else {
+          return tutorial;
+        }
+      });
+
+    case DELETE_TUTORIAL:
+      return tutorials.filter(({ id }) => id !== payload.id);
+
+    case DELETE_ALL_TUTORIALS:
+      return [];
+
+    case RESET_ALL_TUTORIALS:
+      return payload;
+
+    default:
+      return tutorials;
+  }
+}
+
+export default tutorialReducer;
